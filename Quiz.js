76bc54d1d2bedb25f796a5c3b8f6fdae5dd08e39
stewartbrian23 <njugunabriian@gmail.com>
2024-04-27 12:07:42 +0300

    created functions for starting the quiz,showing the question,created variables inside the show questions function

--- Quiz.js
+++ Quiz.js
@@ -43,3 +43,23 @@ const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
 let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();    
+}
+function showQuestion() {
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+    
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+    });
+    
+}
