40739cfe8077ad8887dda198b8887f40fdfcd069
stewartbrian23 <njugunabriian@gmail.com>
2024-04-27 11:29:28 +0300

    created a questions functions and added questions and their answers

---
+++ Quiz.js
@@ -0,0 +1,38 @@
+const questions = [
+    {
+        question:"What is Java?",
+        answers:[
+            {text: "A coffee joint",correct: false},
+            {text: "A famous artist in Europe",correct: false},
+            {text: "A programming language",correct: true},
+            {text: "The best Netflix series",correct: false},
+        ]
+    },
+    {
+        question:"Which of this is a data type in Java?",
+        answers:[
+            {text: "Data typography",correct: false},
+            {text: "Float",correct: true},
+            {text: "Safaricom data",correct: false},
+            {text: "database",correct: false},
+        ]
+    },
+    {
+        question:"Which data structure is mostly taught in schools?",
+        answers:[
+            {text: "Hashtables",correct: false},
+            {text: "ArrayList",correct: false},
+            {text: "Sorting argorithms",correct: false},
+            {text: "Arrays",correct: true},
+        ]
+    },
+    {
+        question:"Why is Java used in most organisations?",
+        answers:[
+            {text: "It is compatible with most devices",correct: true},
+            {text: "It is secure",correct: false},
+            {text: "It is easy to write",correct: false},
+            {text: "Object Oriented",correct: false},
+        ]
+    }
+]
